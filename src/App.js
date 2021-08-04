d900ccfb0c7dc785b76072eed24b81e513e407cf
densus100 <37259694+Densus100@users.noreply.github.com>
2021-08-04 22:24:59 +0700

    Making Column Grid, Install material UI and making floating button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {Container, Row, Col} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Fab } from '@material-ui/core';
+import {Add} from '@material-ui/icons'
+
+
+// npm install -save styled components
+// npm install react-bootstrap@next bootstrap@5.0.2
+
+// npm install @material-ui/icons
+// npm install @material-ui/core
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Row className='My-primary'>
+        <Col xs lg="2" className='border-end border-white '></Col>
+        <Col lg>
+          <div className='My-fab-wrapper'>
+            <Fab className='My-fab' aria-label="add">
+              <Add />
+            </Fab>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
